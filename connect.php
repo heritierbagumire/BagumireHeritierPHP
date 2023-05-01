c41bed39e4442fb1f96caffa4e2ae7ca3d309fcb
heritierbagumire <heritierbagumire367@gmail.com>
2023-05-02 00:04:34 +0200

    feat: upates

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+$severname = 'localhost';
+$username ='root';
+$password = '';
+$database = 'student_db';
+
+$conn = new mysqli($severname,$username,$password,$database);
+if(!$conn){
+    // die(mysqli_error($conn));
+    echo "connection failed!!!";
+
+}
+else{
+
+    // echo "connection successfully !!!!!";
+}
+
+
+?>
